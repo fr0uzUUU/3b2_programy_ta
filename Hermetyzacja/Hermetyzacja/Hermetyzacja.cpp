c48858e9585deb837f68a5f72821b87aa159b022
Michal <michal.ogorek0@gmail.com>
2021-10-22 13:29:42 +0200

    hermetyzacja

---
+++ Hermetyzacja/Hermetyzacja/Hermetyzacja.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+class Prostokat {
+private:
+    double bok1{ 0 }, bok2{ 0 };
+public:
+    Prostokat();
+    Prostokat(double, double);
+    void setBok(double, double);
+    double getBok1();
+    double getBok2();
+    void pobierzBoki(double&, double&);
+};
+
+int main()
+{
+    double dlugosc = 1, szerokosc = 2;
+
+    Prostokat prostokat;
+    prostokat.setBok(5, 7);
+    prostokat.pobierzBoki(dlugosc, szerokosc);
+    
+    cout << prostokat.getBok1() << " " << prostokat.getBok2();
+}
+
+Prostokat::Prostokat()
+{
+}
+
+Prostokat::Prostokat(double pBok1, double pBok2)
+{
+    bok1 = pBok1;
+    bok2 = pBok2;
+}
+
+void Prostokat::setBok(double pBok1, double pBok2)
+{
+    bok1 = pBok1;
+    bok2 = pBok2;
+}
+
+double Prostokat::getBok1()
+{
+    return bok1;
+}
+
+double Prostokat::getBok2()
+{
+    return bok2;
+}
+
+void Prostokat::pobierzBoki(double& pBok1, double& pBok2)
+{
+    pBok1 = bok1;
+    pBok2 = bok2;
+}
---
+++ Hermetyzacja/Zadanie/Zadanie.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+class Prostopadloscian {
+private:
+    int a, b, h, o, p, k;
+public:
+    Prostopadloscian();
+    Prostopadloscian(int, int, int, int, int, int);
+    void setBoki(int, int, int);
+    int obliczObjetosc();
+    int obliczPole();
+    int obliczKrawedzie();
+};
+
+int main()
+{
+    int objentosc = 0, pole = 0, dlugosc = 0;
+    Prostopadloscian prostopadloscian;
+    prostopadloscian.setBoki();
+    prostopadloscian.obliczPole();
+    prostopadloscian.obliczObjetosc();
+    prostopadloscian.obliczKrawedzie();
+}
+
+Prostopadloscian::Prostopadloscian()
+{
+    a = 1;
+    b = 2;
+    h = 3;
+    o = 0;
+    p = 0;
+    k = 0;
+}
+
+Prostopadloscian::Prostopadloscian(int pa, int pb, int ph, int po, int pp, int pk)
+{
+    a = pa;
+    b = pb;
+    h = ph;
+    o = po;
+    p = pp;
+    k = pk;
+}
+
+void Prostopadloscian::setBoki(int, int, int)
+{
+    cin >> a;
+    cin >> b;
+    cin >> h;
+}
+
+int Prostopadloscian::obliczObjetosc()
+{
+    return o = a * b * h;
+}
+
+int Prostopadloscian::obliczPole()
+{
+    return (4 * a * b) + (4 * a * h) + (4 * h * b);
+}
+
+int Prostopadloscian::obliczKrawedzie()
+{
+    return 4 * (a + b + h);
+}
