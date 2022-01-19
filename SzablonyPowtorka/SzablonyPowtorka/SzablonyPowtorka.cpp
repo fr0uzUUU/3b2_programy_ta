2d3fd346267c24ffe20a9a9bb228cce5501da842
Michal <michal.ogorek0@gmail.com>
2022-01-19 10:54:57 +0100

    szablokny

---
+++ SzablonyPowtorka/SzablonyPowtorka/SzablonyPowtorka.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+template <typename T> // dzieki temu nie trzeba podawac typu danych
+T pole(T bok1, T bok2) { // za T można podać typ dancyh, np.:T pole(int bok1, T bok2)
+	T p = bok1 * bok2;
+	return p;
+}
+
+template<>
+double pole(double bok1, double bok2) {
+	double p = bok1 * bok2;
+	return p;
+}
+
+template <typename T>
+void  wypisz(T pole) {
+	cout << pole << endl;
+}
+
+int main()
+{
+	cout << pole(5, 7) << endl;
+	wypisz(pole(5, 7));
+}
---
+++ SzablonyPowtorka/SzablonyPowtorka2/SzablonyPowtorka2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+template <class T>
+class Prostokat {
+public:
+	T bok1, bok2;
+	T pole() {
+		return bok1 * bok2;
+	}
+	T obwod();
+};
+
+template<>class
+Prostokat <double> {
+public:
+	double bok1, bok2;
+	double pole() {
+		return bok1 * bok2;
+	}
+	double obwod() {
+		return 2 * bok1 + 2 * bok2;
+	}
+};
+
+int main()
+{
+	Prostokat<float> p1{ 4,7 };
+	cout << p1.pole() << endl;
+	cout << p1.obwod() << endl;
+	Prostokat<double> p2{ 4.0,7.0 };
+	cout << p2.pole() << endl;
+	cout << p2.obwod() << endl;
+}
+
+template<class T>
+T Prostokat<T>::obwod()
+{
+	return 2 * bok1 + 2 * bok2;
+}
